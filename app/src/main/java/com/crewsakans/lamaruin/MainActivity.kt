4a5343282d7147a6efb52323c9aeb0e292846f4f
Figo <figo.alsistn19@mhs.uinjkt.ac.id>
2022-10-02 10:26:19 +0700

    first commit

---
+++ app/src/main/java/com/crewsakans/lamaruin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.crewsakans.lamaruin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
