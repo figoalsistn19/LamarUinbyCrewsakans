0fdc85d48869e0fc3152fa7fe9d215279f8150cb
Figo <figo.alsistn19@mhs.uinjkt.ac.id>
2022-11-29 08:36:55 +0700

    temporary commit

---
+++ app/src/main/java/com/pmsi/lamaruin/login/asrecruiter/LoginRecruiterActivity.kt
@@ -0,0 +1,12 @@
+package com.pmsi.lamaruin.login.asrecruiter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pmsi.lamaruin.R
+
+class LoginRecruiterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_recruiter)
+    }
+}
---
+++ app/src/main/java/com/pmsi/lamaruin/ui/recruiter/listJob/ListJobFragment.kt
@@ -0,0 +1,18 @@
+package com.pmsi.lamaruin.ui.recruiter.listJob
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pmsi.lamaruin.R
+
+class ListJobFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_job2, container, false)
+    }
+}
