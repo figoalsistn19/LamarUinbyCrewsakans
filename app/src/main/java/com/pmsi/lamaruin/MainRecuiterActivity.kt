d23a0c1ce450d638e40dd21db7201a1107f98b70
Figo <figo.alsistn19@mhs.uinjkt.ac.id>
2022-11-29 08:36:55 +0700

    temporary commit

---
+++ app/src/main/java/com/pmsi/lamaruin/MainRecuiterActivity.kt
@@ -0,0 +1,11 @@
+package com.pmsi.lamaruin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainRecuiterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_recuiter)
+    }
+}
