59b1ff9d3201a7b829ada6466f52cac06fdc8b40
Figo <figo.alsistn19@mhs.uinjkt.ac.id>
2022-12-08 01:44:09 +0700

    commit test

---
+++ app/src/main/java/com/pmsi/lamaruin/ui/recruiter/profile/InputJobActivity.kt
@@ -0,0 +1,41 @@
+package com.pmsi.lamaruin.ui.recruiter.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.pmsi.lamaruin.R
+
+class InputJobActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_job)
+        // access the items of the list
+        val languages = resources.getStringArray(R.array.Languages)
+
+        // access the spinner
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, languages)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    Toast.makeText(this@InputJobActivity,
+                        getString(R.string.selected_item) + " " +
+                                "" + languages[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+    }
+}
