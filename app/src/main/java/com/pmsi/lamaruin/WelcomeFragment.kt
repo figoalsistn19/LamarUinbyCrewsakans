da093c96e896821f1cb11c8a554cfbb08a552686
Figo <figo.alsistn19@mhs.uinjkt.ac.id>
2022-12-12 00:37:33 +0700

    commit 2

---
+++ app/src/main/java/com/pmsi/lamaruin/WelcomeFragment.kt
@@ -0,0 +1,40 @@
+package com.pmsi.lamaruin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.pmsi.lamaruin.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_loginStudentActivity)
+        )
+
+        binding.button2.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_loginRecruiterActivity)
+        )
+    }
+}
