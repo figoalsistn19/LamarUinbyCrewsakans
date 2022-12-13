6627212b67f3c926856b5047ad65d75557ff9173
figoalsistn19 <figo.alsistn19@mhs.uinjkt.ac.id>
2022-12-14 03:00:40 +0700

    InputJobActivity done

--- app/src/main/java/com/pmsi/lamaruin/data/model/JobVacancy.kt
+++ app/src/main/java/com/pmsi/lamaruin/data/model/JobVacancy.kt
@@ -2,9 +2,9 @@ package com.pmsi.lamaruin.data.model
 
 data class JobVacancy constructor(
     var id_job: String = "",
-    var company_name: String?= "",
-    var company_desc: String = "",
+    var job_name: String = "",
     var job_desc: String = "",
-    var qualification: String = "",
-    var job_field: String = ""
+    var job_category: String = "",
+    var qualification: String = ""
+
 )
--- app/src/main/java/com/pmsi/lamaruin/ui/recruiter/profile/InputJobActivity.kt
+++ app/src/main/java/com/pmsi/lamaruin/ui/recruiter/profile/InputJobActivity.kt
@@ -1,5 +1,6 @@
 package com.pmsi.lamaruin.ui.recruiter.profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,14 +8,29 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.core.view.isVisible
+import com.pmsi.lamaruin.MainMahasiswaActivity
+import com.pmsi.lamaruin.MainRecuiterActivity
 import com.pmsi.lamaruin.R
+import com.pmsi.lamaruin.data.LoginPref
+import com.pmsi.lamaruin.data.model.CreateStudent
+import com.pmsi.lamaruin.data.model.JobVacancy
+import com.pmsi.lamaruin.data.remote.FirestoreService
 import com.pmsi.lamaruin.databinding.ActivityInputJobBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
 
 class InputJobActivity : AppCompatActivity() {
 
-    private var posisi_user: String? = "Agent"
+    private var posisi_user: String? = "Marketing"
 
     private lateinit var binding: ActivityInputJobBinding
+
+    @Inject
+    lateinit var service: FirestoreService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInputJobBinding.inflate(layoutInflater)
@@ -39,5 +55,46 @@ class InputJobActivity : AppCompatActivity() {
                 // write code to perform some action
             }
         }
+        binding.btnSubmitJob.setOnClickListener { inputJob() }
+
     }
+    private fun inputJob() {
+
+        val jobName = binding.etJobName.text.toString()
+        val jobDesc = binding.etJobDesc.text.toString()
+        val qualification = binding.etQualification.text.toString()
+        val jobCategory = posisi_user.toString()
+
+        when {
+            jobName.isEmpty() -> {
+                binding.etJobName.error = "Masukan Job Name"
+            }
+            jobDesc.isEmpty() -> {
+                binding.etJobDesc.error = "Masukkan Job Description"
+            }
+            qualification.isEmpty() -> {
+                binding.etQualification.error = "Masukkan Qualification"
+            }
+
+            else -> {
+                val job = JobVacancy(
+                    job_name = jobName,
+                    job_desc = jobDesc,
+                    job_category = jobCategory,
+                    qualification = qualification
+                )
+                service.addJob(job) { id ->
+                    Toast.makeText(
+                        this@InputJobActivity,
+                        "Berhasil Submit Job",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    val intent = Intent(this, MainRecuiterActivity::class.java)
+                    startActivity(intent)
+                }
+                }
+
+            }
+        }
 }
